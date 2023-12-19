3da3375ebd5e4a41bbf6084c73b7b3a3f59c2dbf
Pedro Pinto <pmap@ua.pt>
2023-12-19 21:20:31 +0000

    small

--- run/clean.sh
+++ run/clean.sh
@@ -4,8 +4,9 @@ rm -f error*
 rm -f core
 
 # change 0x61066137 to your semaphore and shared memory key
+
 # Jorge: 0x610683c9
 # Pedro: 0x6106435e
 
-ipcrm -S 0x610683c9
-ipcrm -M 0x610683c9
+ipcrm -S 0x6106435e
+ipcrm -M 0x6106435e
